911fcecd1203078c98e617c6298483b6939334a2
braelynmuskwe <braemira1@icloud.com>
2018-09-04 21:35:38 +0000

    automatically backed up by learn

---
+++ greeting.rb
@@ -0,0 +1,6 @@
+def greeting (local_greeting, your_name, my_name, time_of_day)
+  puts "#{local_greeting} #{your_name}, I am #{my_name}, how's yours #{time_of_day}?"
+  
+end
+
+greeting("hey", "Esmy", "Braelyn", "morning")
